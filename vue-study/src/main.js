982108c3492de2054c9ae9b698ce556e77b5948c
groof <18214147@qq.com>
2020-01-09 13:23:22 +0800

    add change

--- vue-study/src/main.js
+++ vue-study/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router' // 引入路由
-import '../dist/bootstrap.css'
+import '../src/assets/bootstrap.css'
 import '../src/filter/index'
 import '../src/config/config'
 
