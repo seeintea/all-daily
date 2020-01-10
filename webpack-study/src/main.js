28ac032e9dd1efd50dda9a242f99c2cb9dc1dc11
groof <18214147@qq.com>
2020-01-10 20:36:43 +0800

    add webpack

--- webpack-study/src/main.js
+++ webpack-study/src/main.js
@@ -1,8 +1,14 @@
 //  导入 JQUERY
 import $ from 'jquery';
+// import 处理 CSS 样式表
+// webpack 默认打包处理 JS 类型文件，无法处理非 JS 文件
+// 处理非 JS 文件 需要手动安装一些适合的三方 loader 加载器
+// 打包 CSS 文件 需要 npm i style-loader css-loader -D
+import './css/index.css';
+import './css/index.scss';
 
 $(function() {
-    $('li:odd').css('backgroundColor', 'lightgreen');
+    $('li:odd').css('backgroundColor', 'lightGreen');
     $('li:even').css('backgroundColor', function() {
         return '#' + 'B20000';
     })
--- webpack-study/webpack.config.js
+++ webpack-study/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+//导入在内存中生成 html 页面的插件
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     mode: 'development',
     // 入口文件 表示需要打包的文件
@@ -8,10 +11,35 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist/'),
         filename: 'bundle.js'
-    }
+    },
     //自动打包
     //npm i webpack-dev-server -D
     //安装到本地项目依赖
     //打包的文件没有存放在物理磁盘中
     //托管在电脑内存中
+    // webpack-dev-server 第二种配置方法
+    // devServer: {
+    //     open: true,
+    //     port: 4000,
+    //     contentBase: 'src',
+    //     hot: true
+    // }
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, 'src/index.html'), //指定模版页面
+            filename: 'index.html' //指定生成页面名称
+        })
+    ],
+    // 配置三方模块加载器
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    }
 }
