c9fffb8e059605b1c1d2f4d82377b686db29313c
groof <18214147@qq.com>
2020-02-05 13:54:11 +0800

    fix bug

--- vue-music/src/base/slider/slider.vue
+++ vue-music/src/base/slider/slider.vue
@@ -35,12 +35,13 @@ export default {
         _setSliderWidth() {
             this.children = this.$refs.sliderGroup.children;
             let width = 0;
-            let sliderWidth = this.$refs.slider;
+            let sliderWidth = this.$refs.slider.clientWidth;
             for(let i=0; i<this.children.length; i++){
                 let child = this.children[i]
                 addClass(child, 'slider-item');
                 child.style.width = sliderWidth + 'px';
                 width += sliderWidth; 
+                
             } 
             if(this.loop){
                 width += 2*sliderWidth;
--- vue-music/src/utils/dom.js
+++ vue-music/src/utils/dom.js
@@ -10,6 +10,6 @@ export function addClass(el, className) {
 
 // 20200204 problem
 export function hasClass(el, className) {
-    let reg = new RegExp('^|\\s' + className + '^|\\s');
+    let reg = new RegExp('(^|\\s)' + className + '(^|\\s)');
     return reg.test(el.className);
 }
