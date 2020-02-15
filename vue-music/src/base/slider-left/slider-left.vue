ad83deebec8276445be82718abb814b0e8468ee7
groof <18214147@qq.com>
2020-02-15 21:20:39 +0800

    add slider-left

--- vue-music/src/base/slider-left/slider-left.vue
+++ vue-music/src/base/slider-left/slider-left.vue
@@ -9,13 +9,22 @@
 
 <script>
 
-// import BScroll from 'better-scroll'
+//import BScroll from 'better-scroll'
 
 export default {
     mounted() {
         setTimeout(()=>{
             this._setImgWidth()
+            this._initSlider()
         }, 20)
+
+        // 刷新
+        window.addEventListener("resize", ()=>{
+			if(!this.slider){
+				return
+			} 
+			this._setImgWidth();
+		})
     },
     methods:{
         _setImgWidth(){
@@ -29,12 +38,15 @@ export default {
                 child.style.width = itemWidth + 'px'
                 child.style.marginRight = '10px'
                 width += itemWidth + 10;
-                // let childImg = child.getElementsByTagName("img")[0]
-                // childImg.style.width = itemWidth + 'px';
-                // width += itemWidth + 10; 
             } 
             this.$refs.sliderGroup.style.width = width + 'px ';
-        }
+        },
+
+        _initSlider () {
+            // 左右滑动利用margin控制
+            // 注意左右极限
+            // 判断左右滑动
+		},
     }
 }
 </script>
@@ -42,13 +54,25 @@ export default {
 <style lang="scss" scoped>
 .slider {
     padding: 0 10px;
+    position: relative;
     .slider-group{
         display: flex;
+        position: relative;
+        margin: 5px 0 0 0;
         img {
             width: 100%;
+            border-radius: 10px;
         }
         p {
-            font-size: 9px;
+            margin: 3px 0 0 0;
+            font-size: 10px;
+            text-overflow: -o-ellipsis-lastline;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            line-clamp: 2;
+            -webkit-box-orient: vertical;
         }
     }
 }
