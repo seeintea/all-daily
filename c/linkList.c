c4a0eea9bf6e6080c0a1108cdd973b706ac8fb1c
groof <18214147@qq.com>
2020-02-28 23:12:31 +0800

    bug fix

--- c/linkList.c
+++ c/linkList.c
@@ -27,6 +27,7 @@ void init(struct Node* node){
 
 // 创建新节点
 // 返回新节点指针
+// test pass
 struct Node* createNode(var data){
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
     node->data = data;
@@ -36,6 +37,7 @@ struct Node* createNode(var data){
 
 // 顺序插入
 // 返回节点插入的位置
+// test pass
 int append(struct Node* node, var data){
     struct Node* newNode = createNode(data);
     int count = 1;
@@ -48,50 +50,81 @@ int append(struct Node* node, var data){
 }
 
 // 制定位置插入
-// 返回被替换的节点的值
-var insert(struct Node* node, var data, int index){
-    // 查询位置是否符合要求
-    if(!index) return (var)-1;
-    int count = 0;
+// not pass
+bool insert(struct Node* node, var data, int index){
+    // 判断索引是否负越界
+    if(index <= 0) return false;
+    // 寻找插入位置并判断是否负越界
     struct Node* rear = node->next;
-    while (rear && count < index-2) {
+    int count = 1;
+    while(rear && count++ < index-1)
         rear = rear->next;
-        count++;
-    }
+    // 如果 rear 为空，则正越界
     if(!rear)
-        return (var)-1;
-    var bup = rear->data;
-    rear->data = data;
-    return bup;
+        return false;
+    // 如果 index == 1，插入节点为头节点
+    if(index == 1){
+        // 创建需要插入的节点
+        struct Node* newNode = createNode(data);
+        newNode->next = node->next;
+        node->next = newNode;
+        return true;
+    }
+    // 正常插入节点
+    // 考虑细节 -> 节点是否有后继节点
+    struct Node* temp = rear->next;
+    // 无后继节点
+    if(!temp){
+        temp = createNode(data);
+        rear->next = temp;
+        return true;
+    }
+    temp = createNode(data);
+    temp->next = rear->next;
+    rear->next = temp;
+    return true;
 }
 
 // 移除某个节点
 // 返回被移除的节点的值
+// test pass
 var remove(struct Node* node, int index){
-    // 查询位置是否符合要求
-    if(!index) return (var)-1;
-    int count = 0;
+    var bup = -1;
+    // 判断索引是否负越界
+    if(index <= 0) return bup;
+    // 判断索引是否正越界
     struct Node* rear = node->next;
-    while (rear && count < index-1) {
+    int count = 1;
+    // 跳出循环的条件
+    // 当前节点为空 -> 正越界
+    // count <= index index在符合添加的范围内[ 1 - _size ]
+    // 移除节点需要找到被移除节点的前置节点
+    while(rear && count++ < index-1)
         rear = rear->next;
-        count++;
+    // 如果 rear 为空，则正越界
+    if(!rear)
+        return bup;
+    // 如果 index == 1，头节点指向下一位
+    if(index == 1){
+        // 释放内存并将链表的元素头指针移位
+        struct Node temp;
+        temp.next = node->next;
+        node->next = rear->next;
+        bup = temp.next->data;
+        free(temp.next);
+        return bup;
     }
-    var bup = rear->data;
-    printf("|%d|\n", bup);
-    // 创建临时节点
-    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-    // 绑定要删除的节点
-    temp = rear;
-    // 下一节点不为空
-    if(rear->next){
-        printf("-%d-\n", rear->next->data);
-        rear = rear->next;
-    }
-    free(temp->next);
+    // 正常删除节点
+    struct Node temp;
+    temp.next = rear->next;
+    rear->next = rear->next->next;
+    bup = temp.next->data;
+    free(temp.next);
     return bup;
 }
 
 // 打印所有的节点
+// test pass
 void printNode(const struct Node* node){
     struct Node* rear = node->next;
     while (rear) {
@@ -102,6 +135,7 @@ void printNode(const struct Node* node){
 }
 
 int main(int argc, const char * argv[]){
+    /* testing */
     struct Node* list = (struct Node*)malloc(sizeof(struct Node));
     init(list);
     printf("%d\n", append(list, 12));
@@ -110,14 +144,13 @@ int main(int argc, const char * argv[]){
     printf("%d\n", append(list, 20));
     printf("%d\n", append(list, 22));
     printNode(list);
-    printf("%d\n", insert(list, 10, 1));
-    printf("%d\n", insert(list, 11, 2));
-    printf("%d\n", insert(list, 12, 3));
-    printf("%d\n", insert(list, 13, 4));
-    printf("%d\n", insert(list, 13, 7));
-    printf("%d\n", insert(list, 14, 5));
-    printf("%d\n", insert(list, 13, 9));
+    printf("%d\n", remove(list, 1));
+    printf("%d\n", remove(list, 4));
+    printf("%d\n", remove(list, 5));
     printNode(list);
-    remove(list, 2);
+    printf("%d\n", insert(list, 12, 4));
+    printf("%d\n", insert(list, 22, 1));
+    printf("%d\n", insert(list, 26, 7));
     printNode(list);
+    /* testing */
 }
